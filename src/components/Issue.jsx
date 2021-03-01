f9f315162f1012218a93ff5aa7039c5817c7bf7e
Terrence Childs <tchilds1108@gmail.com>
2021-03-01 14:37:40 -0500

    second

--- src/components/Issue.jsx
+++ src/components/Issue.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import ReactMarkdown from "react-markdown/with-html";
 
 const Issue = (props) => {
     const { issue } = props;
     return (
       <li>
-        Title: {issue.title}
-        <a href={issue.url} >
-         Link:  {issue.url}
-        </a>
-      </li>
-    );
+        <h2>{issue.title}</h2>
+        <p>
+            <a href={issue.url} >{issue.url}</a>
+        </p>
+        <ReactMarkdown children={issue.body} />
+      </li>)
   };
   
   export default Issue;
